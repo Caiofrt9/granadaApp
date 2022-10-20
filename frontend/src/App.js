5b67c897b25580ed747d3ebd7f9c43d22bb71bbb
Caio F <silvacaio295@gmail.com>
2022-10-20 11:30:45 -0300

    creating an authhook and adding navbar and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,50 @@
 import './App.css'
 
+//Router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+//Hooks
+import { useAuth } from './hooks/useAuth'
 
 //Pages
 import Home from './pages/Auth/Login'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 
+//Components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 function App() {
+  const { auth, loading } = useAuth()
+
+  console.log(loading)
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Home /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
+          </Routes>
+          <Footer />
+        </div>
       </BrowserRouter>
     </div>
   )
